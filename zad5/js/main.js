fa59b846a62a39f180ea3d4609364c4f627b845d
srupakij <srupakij731@gmail.com>
2023-10-24 22:51:17 +0200

    a

--- zad5/js/main.js
+++ zad5/js/main.js
@@ -1,5 +1,19 @@
-var a = prompt("podaj 1 liczbe")
-var b = prompt("podaj 2 liczbe")
-document.write("Obliczający wynikrównania x = a/b.") 
-document.write('<br>')
-document.write(a/b)
+function updateClock(){
+    var today = new Date();
+ var day = String(today.getDate());
+ var month = String(today.getMonth()+1);
+ var year = String(today.getFullYear());
+
+ var hour = String(today.getHours());
+ if(hour < 10)
+    hour = "0"+hour;
+ var minute = String(today.getMinutes());
+ if(minute < 10)
+    minute = "0"+ minute;
+ var second = String(today.getSeconds());
+if(second < 10)
+    second = "0" + second;
+ document.getElementById("date").textContent = day + " : " + month + " : " + year;
+ document.getElementById("hour").textContent = hour + " : "+ minute + " : "+ second
+}
+setInterval(updateClock, 1000);
